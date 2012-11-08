ffcfccd12e08f4b0dcfeb7859051d2d3f0646e59
Jan Wagner <waja@cyconet.org>
2012-11-08 16:48:20 +0100

    add creation of database

--- examples/heating_structure.sql
+++ examples/heating_structure.sql
@@ -1,23 +1,20 @@
--- phpMyAdmin SQL Dump
--- version 3.3.7deb7
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Nov 08, 2012 at 11:20 AM
--- Server version: 5.1.63
--- PHP Version: 5.3.3-7+squeeze14
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+CREATE USER 'heating'@'localhost' IDENTIFIED BY  'f00b4r';
+GRANT USAGE ON * . * TO  'heating'@'localhost' IDENTIFIED BY  'f00b4r' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+CREATE DATABASE IF NOT EXISTS  `heating` ;
+GRANT ALL PRIVILEGES ON  `heating` . * TO  'heating'@'localhost';
+
 --
 -- Database: `heating`
 --
+
+USE heating;
 
 -- --------------------------------------------------------
 
