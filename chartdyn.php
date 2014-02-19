e8ccc134af21222a014b0d77e681bcd13a33f0f4
Marcel Dumont <marcel@globaltopmedia.com>
2014-02-19 20:58:34 +0000

    added json-driven highcharts

---
+++ chartdyn.php
@@ -0,0 +1,105 @@
+<html>
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="http://code.highcharts.com/stock/highstock.js"></script>
+<script src="http://code.highcharts.com/stock/modules/exporting.js"></script>
+</head>
+<body>
+<div id="container" style="width:100%; height:400px;"></div>
+<script>
+var chart; // global
+
+$(function () {
+
+    var seriesOptions = [],
+	colors = Highcharts.getOptions().colors;
+        names = ['avgtemp', 'actualtemp'];
+
+    seriesCounter = 0;
+
+    $.each(names, function (i, name) {
+        $.getJSON("jsonchart.php?callback=?&context=" + name.toLowerCase(), function (data) {
+            seriesOptions[i] = {
+                name: name,
+                data: data
+            };
+            seriesCounter++;
+            if (seriesCounter == names.length) {
+		createChart();
+            }
+        });
+    });
+
+
+    function createChart() {
+        $('#container').highcharts('StockChart', {
+
+            rangeSelector: {
+		        buttons: [
+{
+                            type: 'day',
+                            count: 1,
+                            text: '1d'
+                        },{
+		            type: 'day',
+		            count: 3,
+		            text: '3d'
+		        }, {
+		            type: 'week',
+		            count: 1,
+		            text: '1w'
+		        }, {
+		            type: 'month',
+		            count: 1,
+		            text: '1m'
+		        }, {
+		            type: 'month',
+		            count: 6,
+		            text: '6m'
+		        }, {
+		            type: 'year',
+		            count: 1,
+		            text: '1y'
+		        }, {
+		            type: 'all',
+		            text: 'All'
+		        }],
+		        selected: 3
+		    },
+
+		yAxis: {
+			title: {
+				text: 'Temperature (°C)'
+			}
+		},
+
+
+            title: {
+                text: 'Temperature'
+            },
+
+			subtitle: {
+				text: 'Built chart at...' // dummy text to reserve space for dynamic subtitle
+			},
+
+		plotOptions: {
+		    	series: {
+//		    		compare: 'percent'
+		    	}
+		    },
+
+
+	tooltip: {
+		    	pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>', //({point.change}%)
+		    	valueDecimals: 1
+	},
+            series: seriesOptions
+        });
+    }
+
+});
+</script>
+
+
+</body>
+</html>
---
+++ jsonchart.php
@@ -0,0 +1,74 @@
+<?php
+header("Content-type: text/json");
+
+/*
+Display the collected (and stored) data by the poller via ycharts
+*/
+
+//includes
+include "includes/config.php";
+// database config
+include "includes/sql_cred.php";
+// database query
+
+//$timeframe = 3600;
+
+switch ($_GET["context"]) {
+	case timing:
+		$query = "SELECT `timestamp`, `67`, `68`, `70`, `71`, `72`, `73`, `74`, `75`, `77`, `141` FROM `$mytimetable`WHERE timestamp > ((SELECT UNIX_TIMESTAMP()) - $timeframe)";
+		break;
+	case operation:
+	case impulse:
+		$query = "SELECT `timestamp`, `56`, `57`, `60`, `63`, `64`, `65` FROM `$myoptable`WHERE timestamp > ((SELECT UNIX_TIMESTAMP()) - $timeframe)";
+		break;
+	default:
+		$query = "SELECT (`timestamp`*1000) as 'timestamp',  ((`15`)*0.1) as 'actualtemp',  ((`16`)*0.1) as 'avgtemp' FROM `$mytemptable`WHERE timestamp > ((SELECT UNIX_TIMESTAMP()) - $timeframe)";
+	}
+
+//Connect to the database. (host,username,password,database)
+$mysqli = mysqli_connect($myhost, $myuser, $mypasswd, $mydatabase);
+// Check for errors connecting to database.
+if (mysqli_connect_errno()) {
+        die('Unable to connect to database. '.$mysqli -> connect_error);
+}
+try {
+        // All queries and commands go here.
+	// multi query
+        if(!$mysqli -> multi_query($query)) {
+		// get exception
+                throw new Exception($mysqli -> error);
+        } else {
+		do {
+			// get results
+			if ($result = $mysqli->use_result()) {
+				while ($row = $result->fetch_array(MYSQLI_ASSOC)) { 
+					switch ($_GET["context"]) {
+					        case avgtemp:
+							$data[] = "[$row[timestamp], $row[avgtemp]]";
+							break;
+						case actualtemp:
+							$data[] = "[$row[timestamp], $row[actualtemp]]";
+							break;
+						default:
+							$data[] = "[$row[timestamp], $row[actualtemp], $row[avgtemp]]";
+						}
+			//		$data[] = "[$row[timestamp], $row[actualtemp], $row[avgtemp]]";
+				}
+                                $result->close();
+                        }
+                } while ($mysqli->next_result());
+
+
+      $json = $_GET['callback'] . '('.json_encode($data).');';  //see note below regarding this line.                                   
+      echo str_replace('"', '', $json);  //echos string with quotes removed.
+
+	}
+}
+// display error message if is any
+catch (Exception $e) {
+        echo $e -> getMessage();
+}
+
+// Close $db connection
+$mysqli->close();
+?>
